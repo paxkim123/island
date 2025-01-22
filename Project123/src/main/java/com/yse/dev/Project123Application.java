087e6927df33d9634590c0b6524cbce9613e61fa
Project123 <kjwcan@naver.com>
2025-01-22 11:28:39 +0900

    a1

---
+++ Project123/src/main/java/com/yse/dev/Project123Application.java
@@ -0,0 +1,13 @@
+package com.yse.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project123Application.class, args);
+	}
+
+}
