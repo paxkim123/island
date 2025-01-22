16fbed4b3acd9b473017b69d84f5d0a1e5cddda7
paxkim123 <kjwcan@naver.com>
2025-01-22 16:06:17 +0900

    Add files via upload

---
+++ Hospital/src/main/java/Hospital/Patient/Service/PatientInfoService.java
@@ -0,0 +1,45 @@
+package Hospital.Patient.Service;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.stereotype.Service;
+
+import Hospital.Patient.DTO.PatientInfoCreateDTO;
+import Hospital.Patient.DTO.PatientInfoReadDTO;
+import Hospital.Patient.Entity.Patient;
+import Hospital.Patient.Entity.PatientInfoRepository;
+
+
+@Service
+public class PatientInfoService {
+	   private PatientInfoRepository pir;
+	   
+	   public PatientInfoService(PatientInfoRepository pir) {
+	      this.pir = pir;
+	   }
+	   public Integer PatientInfoInsert(PatientInfoCreateDTO picDTO) {
+		      Patient patient_data = Patient.builder()
+		                        .Name(picDTO.getName())
+		                        .Gender(picDTO.getGender())
+		                        .Phone_Number(picDTO.getPhone_Number())
+		                        .Address_1(picDTO.getAddress_1())
+		                        .Address_2(picDTO.getAddress_2())
+		                        .Taking_Pill(picDTO.getTaking_Pill())
+		                        .Nose(picDTO.getNose())
+		                        .Cough(picDTO.getCough())
+		                        .Pain(picDTO.getPain())
+		                        .Diarrhea(picDTO.getDiarrhea())
+		                        .High_Risk_Group(picDTO.getHigh_Risk_Group())
+		                        .VAS(picDTO.getVAS())
+		                        .Agreement(picDTO.getAgreement())
+		                        .build();
+		      this.pir.save(patient_data);
+		      return patient_data.getPatientId();
+		   }
+	   public PatientInfoReadDTO PatientInfoRead(Integer PatientId) throws NoSuchElementException{
+		      Patient p = this.pir.findById(PatientId).orElseThrow();
+		      return PatientInfoReadDTO.PatientInfoFactory(p);
+		   }
+
+
+}
