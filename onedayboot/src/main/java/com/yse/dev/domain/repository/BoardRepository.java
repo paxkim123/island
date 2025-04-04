9fb40e1f94cabb57cf604c9dc7e94f3f924febd5
paxkim123 <kjwcan@naver.com>
2025-04-04 16:22:47 +0900

    Delete onedayboot directory

--- onedayboot/src/main/java/com/yse/dev/domain/repository/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yse.dev.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.yse.dev.domain.entity.Board;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-
-}
