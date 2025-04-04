44c15edb59579821116dadf24d2e0e49585ab22c
paxkim123 <kjwcan@naver.com>
2025-04-04 16:23:46 +0900

    Delete src directory

--- src/main/java/Hospital/Patient/Entity/D_HospitalRepository.java
+++
@@ -1,7 +0,0 @@
-package Hospital.Patient.Entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface D_HospitalRepository extends JpaRepository<D_Hospital, Integer> {
-
-}
--- src/main/java/Hospital/Patient/Entity/PatientInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package Hospital.Patient.Entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientInfoRepository extends JpaRepository<Patient, Integer> {
-
-}
