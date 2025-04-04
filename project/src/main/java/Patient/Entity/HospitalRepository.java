af2ad99f4c416a3630cde809bdf68bce87c45473
paxkim123 <kjwcan@naver.com>
2025-04-04 16:23:37 +0900

    Delete project directory

--- project/src/main/java/Patient/Entity/HospitalRepository.java
+++
@@ -1,9 +0,0 @@
-package Patient.Entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
-
-}
