428d56997199635a49366b592ab0d9c96d907b61
paxkim123 <kjwcan@naver.com>
2025-04-04 16:19:22 +0900

    Delete Reservation directory

--- Reservation/Service/AppointmentRepository.java
+++
@@ -1,9 +0,0 @@
-package Hospital.Reservation.Service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import Hospital.Reservation.Entity.Appointment;
-
-public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-	
-}
